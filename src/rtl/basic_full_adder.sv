d57e10f2af350a7832af98344e171617f7c05fe0
Electro_Insights <162033367+ElectroInsights@users.noreply.github.com>
2024-03-13 12:39:58 +0530

    Create basic_full_adder.sv
    It is a basic full adder with 1-bit input as a, b and c_in.

---
+++ src/rtl/basic_full_adder.sv
@@ -0,0 +1,12 @@
+module basic_full_adder (
+  input  logic a,
+  input  logic b,
+  input  logic c_in,
+  output logic sum,
+  output logic c_out
+);
+  
+  assign sum   = a ^ b ^ c_in;
+  assign c_out = (a && b) || (b && c_in) || (c_in && a);
+  
+endmodule
